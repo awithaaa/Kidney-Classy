c6b1b70439215cfdd35439f432924866c2c9d4c0
Arosha Withanage <avwithanage2006@gmail.com>
2026-02-24 09:17:47 +0530

    feat: add model training

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kidney_classy import logger
 from kidney_classy.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from kidney_classy.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from kidney_classy.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -25,3 +26,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
